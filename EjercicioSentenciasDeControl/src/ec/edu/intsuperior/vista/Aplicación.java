2e285c401d526a90607bf47adaabd6816dae29c3
Nelson <nb.mejia@intsuperior.edu.ec>
2022-01-26 21:16:39 -0500

    "Este proyecto es Nuevo y Editable"

---
+++ EjercicioSentenciasDeControl/src/ec/edu/intsuperior/vista/Aplicación.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+/**
+ *
+ * @author Nelson
+ */
+public class Aplicación {
+
+    public static void main(String[] args) {
+
+        System.out.println("Este es mi Proyecto");
+
+    }
+}
